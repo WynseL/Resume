d553c6f3595ed1074980023334d470ab20a4c5d1
Chad Gonzales <gonzales@mclinica.com>
2019-09-02 20:25:38 +0800

    * Initial commit

---
+++ resume/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter_web/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Main',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Main Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Welcome boi!',),
+          ],
+        ),
+      ),
+    );
+  }
+}
