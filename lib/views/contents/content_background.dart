a0bb63125c904245e620399db0a64546ba087b1c
Chad Gonzales <gonzales@mclinica.com>
2019-10-19 09:25:20 +0800

    * Removed legacy home page class * Updated design for background

--- lib/views/contents/content_background.dart
+++ lib/views/contents/content_background.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_web/material.dart';
 import 'package:flutter_web/widgets.dart';
+import 'package:resume/utils/constants.dart';
 import 'package:resume/utils/default-colors.dart';
 
 class BackgroundContent extends StatefulWidget {
@@ -8,56 +9,98 @@ class BackgroundContent extends StatefulWidget {
 }
 
 class BackgroundContentState extends State<BackgroundContent> {
+
+  _timelineItem() {
+
+    double cardHeight = 100.0;
+    double fixedIconSize = 60.0;
+
+    double fixedIconSizeBorder = fixedIconSize - 10;
+    double fixedIconSizeCenter = (cardHeight / 2) + 10;
+
+    return Stack(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(left: 48),
+          child: Card(
+            margin: EdgeInsets.all(32),
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: cardHeight,
+            ),
+          ),
+        ),
+        Positioned(
+          top: 0.0,
+          left: 35.0,
+          bottom: 0.0,
+          child: Container(
+            height: 50.0,
+            width: 1.0,
+            color: WebColors.frenchPuse,
+          ),
+        ),
+        Positioned(
+          top: fixedIconSizeCenter,
+          left: 10.0,
+          child: Container(
+            height: fixedIconSizeBorder,
+            width: fixedIconSizeBorder,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: WebColors.darkVanilla
+            ),
+            child: Container(
+              margin: EdgeInsets.all(5.0),
+              height: fixedIconSize,
+              width: fixedIconSize,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: WebColors.frenchPuse
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (content, index) {
-        Stack(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(32),
-              child: Card(
-                child: Container(
-                  width: 300.0,
-                  height: 100.0,
-
-                ),
-              ),
+    return 
+    
+    
+    Container(
+      color: WebColors.darkVanilla,
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width - DrawerWidth,
+      padding: EdgeInsets.all(32),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          // Text(
+          //   Background_Title,
+          //   style: TextStyle(
+          //     fontSize: 42.0 
+          //   ),
+          // ),
+          SizedBox(height: 16,),
+          Container(
+            child: ListView(
+              children: <Widget>[
+                _timelineItem(),
+                _timelineItem(),
+                _timelineItem(),
+                _timelineItem(),
+                _timelineItem(),
+                _timelineItem(),
+                _timelineItem(),
+              ],
             ),
-            Positioned(
-              top: 0.0,
-              left: 35.0,
-              bottom: 0.0,
-              child: Container(
-                height: 40.0,
-                width: 1.0,
-                color: WebColors.frenchPuse,
-              ),
-            ),
-            Positioned(
-              top: 100.0,
-              left: 15.0,
-              child: Container(
-                height: 50,
-                width: 50,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: WebColors.cream
-                ),
-                child: Container(
-                  margin: EdgeInsets.all(5.0),
-                  height: 40.0,
-                  width: 40.0,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: WebColors.frenchPuse
-                  ),
-                ),
-              ),
-            )
-          ],
-        );
-      },
+          ),
+        ]
+      ),
     );
   }
 }
