3bd915fdd2b6483b2e8f7d2c1265e57bb6beeb8e
Chad Gonzales <gonzales@mclinica.com>
2019-10-19 09:25:53 +0800

    * Changed some constant texts

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -13,8 +13,10 @@ const Contact_Info_Location = ["Location", "asddasddsad"];
 const Contact_Info_Email = ["Email", "asdadadsadasdasd"];
 const Contact_Info_Mobile = ["Mobile Number", "+19837121309"];
 
-const About_Me_Title = "About Me";
+const About_Me_Title = "About";
 const About_Me_Desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+const Background_Title = "Background";
 
 const Skills_Title = "SKILLS";
 const Skills_1 = ["Mobile", "Java, Kotlin, Objective-C, Swift, C#, Flutter/Dart"];
